e0ff3631fff9fbe9e756f1f56dbded6c512e832e
Jason Mitchell <jason.mitchell.w@gmail.com>
2017-06-27 02:24:55 -0700

    Use unwatchedDir to prevent ember-cli from setting up a watcher on intl-relativeformat node path

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 'use strict';
 
 var path = require('path');
-var mergeTrees = require('broccoli-merge-trees');
 var Funnel = require('broccoli-funnel');
+var UnwatchedDir = require('broccoli-source').UnwatchedDir;
+var mergeTrees = require('broccoli-merge-trees');
 
 var relativeFormatPath = path.dirname(require.resolve('intl-relativeformat'));
 
@@ -11,7 +12,7 @@ module.exports = {
   name: 'intl-relativeformat',
 
   treeForAddon: function(tree) {
-    var messageFormatParserTree = new Funnel(path.join(relativeFormatPath, 'src'));
+    var messageFormatParserTree = new Funnel(new UnwatchedDir(path.join(relativeFormatPath, 'src')));
     var trees = mergeTrees([messageFormatParserTree, tree], { overwrite: true });
 
     return this._super.treeForAddon.call(this, trees);
