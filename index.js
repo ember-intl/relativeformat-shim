2a04c358433a72df0123f083ce422125a832d9d3
Jason Mitchell <jason.mitchell.w@gmail.com>
2017-08-02 21:53:07 -0700

    Upgrading ember-intl-messageformat, remove empty folders

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 'use strict';
 
 const path = require('path');
-const Funnel = require('broccoli-funnel');
 const mergeTrees = require('broccoli-merge-trees');
 const UnwatchedDir = require('broccoli-source').UnwatchedDir;
 
@@ -12,9 +11,9 @@ module.exports = {
   name: 'intl-relativeformat',
 
   treeForAddon(tree) {
-    const relativeFormatTree = new Funnel(new UnwatchedDir(path.join(relativeFormatPath, 'src')));
-    const trees = mergeTrees([relativeFormatTree, tree], { overwrite: true });
+    const relativeFormatTree = new UnwatchedDir(path.join(relativeFormatPath, 'src'));
+    const addonTree = mergeTrees([relativeFormatTree, tree], { overwrite: true });
 
-    return this._super.treeForAddon.call(this, trees);
+    return this._super.treeForAddon.call(this, addonTree);
   }
 };
